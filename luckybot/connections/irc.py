7bedff3c0a1d123133e6c04740de71df36df6e6f
Lucas van Dijk <luckyluke56@gmail.com>
2009-10-23 00:41:01 +0200

    Little start with main bot class

--- luckybot/connections/irc.py
+++ luckybot/connections/irc.py
@@ -36,15 +36,94 @@ class Client(GlibConnection, gobject.GObject):
 		'closed': (gobject.SIGNAL_RUN_LAST, None, ())
 	}
 	
-	def __init__(self, username, password, invisible=True)
+	def __init__(self)
 		"""
 			Constructor, sets some members
 		"""
 		
 		gobject.GObject.__init__(self)
 		GlibConnection.__init__(self, socket.AF_INET, socket.SOCK_STREAM)
-		
-		self.username = username
-		self.password = password
-		self.invisible = invisible
+	
+	def send_pm(self, nick, message):
+		"""
+			Sends a message to a channel or nickname
 
+			@type nick: string
+			@param nick: The channel or nickname to send a message
+
+			@type message: string
+			@param message: The message to send
+		"""
+
+		return self.send("PRIVMSG %s :%s" % (nick, message))
+
+	def send_notice(self, nick, message):
+		"""
+			Sends a notice to a given channel/nickname
+
+			@type nick: string
+			@param nick: The channel or nickname to send a notice
+
+			@type message: string
+			@param message: The message to send
+		"""
+
+		return self.send("NOTICE %s :%s" % (nick, message))
+
+	def send_action(self, dest, message):
+		"""
+			Sends an 'action' message to a given destination
+			Like you use the /me command on a normal IRC client
+
+			@type dest: string
+			@param dest: The nickname or channel you want to send to
+
+			@type message: string
+			@param message: The message
+		"""
+
+		self.send_pm(dest, "\001ACTION %s\001" % message)
+
+	def join(self, channel):
+		"""
+			Joins a given channel
+
+			@type channel: string
+			@param channel: The channel to join
+		"""
+
+		if not channel.startswith('#'):
+			channel = '#%s' % (channel)
+
+		self.send("JOIN %s" % channel)
+
+	def part(self, channel):
+		"""
+			Leaves a given channel
+
+			@type channel: string
+			@param channel: The channel to leave
+		"""
+
+		if not channel.startswith('#'):
+			channel = '#%s' % channel
+
+		self.send("PART %s" % channel)
+
+	def set_nick(self, nickname):
+		"""
+			Changes the nickname
+
+			@type nickname: string
+			@param nickname: The new nickname
+		"""
+
+		self.send("NICK %s" % nickname)
+		self.nickname = nickname
+
+	def kick(self, channel, nickname, reason = ""):
+		"""
+			Kicks a specified user
+		"""
+
+		self.send("KICK %s %s :%s" % (channel, nickname, reason))
